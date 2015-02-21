386704c201f8ec07491330271f5a2347106401d3
Christoph Herrmann <developer@dragonprojects.de>
2015-02-21 12:01:17 +0100

    Upgrade to current version of DragonNode.js AngularJS Skeleton

--- app.js
+++ app.js
@@ -4,23 +4,42 @@
 // Load the libraries and modules
 
 var config = {
-    libraries: {
-        compression: require('compression'),
-        express: require('express')
-    },
     directory: __dirname + '/modules/',
     modules: {
+        npm: [
+            [require('dragonnodejs-express'), {
+                app: {
+                    port: process.env.PORT
+                },
+                auth: {
+                    disabled: process.env.AUTH_DISABLED,
+                    realm: process.env.AUTH_REALM,
+                    user: process.env.AUTH_USER,
+                    password: process.env.AUTH_PASSWORD
+                },
+                header: {
+                    'X-UA-Compatible': 'IE=edge,chrome=1',
+                    'X-Frame-Options': 'DENY',
+                    'X-XSS-Protection': '1; mode=block',
+                    'X-Powered-By': null
+                },
+                static: {
+                    directory: __dirname + '/web/'
+                }
+            }]
+        ],
         directory: {
-            app: {
-                port: process.env.PORT,
-                directory: __dirname + '/web/'
-            },
             config: {
                 route: '/js/config.js',
-                pkg: __dirname + '/package.json',
-                env: {
-                    server: process.env.SERVER
-                }
+                config: function () {
+                    var pkg = require(__dirname + '/package.json');
+                    return {
+                        name: pkg.name,
+                        version: pkg.version,
+                        homepage: pkg.homepage,
+                        server: process.env.SERVER
+                    };
+                }()
             }
         }
     }
--- modules/config.js
+++ modules/config.js
@@ -6,27 +6,22 @@
  * @example
     config: {
         route: '/js/config.js',
-        pkg: __dirname + '/package.json',
-        env: {}
+        config: function () {
+            var pkg = require(__dirname + '/package.json');
+            return {
+                name: pkg.name,
+                version: pkg.version,
+                homepage: pkg.homepage,
+                server: process.env.SERVER
+            };
+        }()
     }
  */
 
 module.exports = function (config, libraries, services) {
     var app = services.app;
 
-    var json = {
-        pkg: function () {
-            var pkg = require(config.pkg);
-            return {
-                name: pkg.name,
-                version: pkg.version,
-                homepage: pkg.homepage
-            };
-        }(),
-        env: config.env
-    };
-    var js = 'var config = ' + JSON.stringify(json) + ';';
-
+    var js = 'var config = ' + JSON.stringify(config.config) + ';';
     app.get(config.route, function (req, res) {
         res.send(js);
     });
