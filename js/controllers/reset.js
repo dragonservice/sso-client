d0a33d77027a9ff6f6f3694790bc67c578b945f6
Christoph Herrmann <developer@dragonprojects.de>
2015-03-10 22:55:19 +0100

    Reset password with token

--- js/controllers/reset.js
+++ js/controllers/reset.js
@@ -9,17 +9,33 @@ angular.module(module.exports, dependencies)
     .config(['$routeProvider',
         function ($routeProvider) {
             $routeProvider
-                .when('/reset', {
+                .when('/reset/:token', {
                     templateUrl: 'views/reset.html',
                     controller: 'ResetCtrl'
                 });
         }
     ])
-    .controller('ResetCtrl', function ($scope) {
+    .controller('ResetCtrl', function ($scope, $http, $routeParams) {
         $scope.user = {};
         $scope.submit = function () {
             $scope.$broadcast('show-errors-check-validity');
             if ($scope.form.$valid) {
+                $http
+                    .post(
+                        config.server + '/reset',
+                        {
+                            token: $routeParams.token,
+                            password: $scope.user.password
+                        }
+                    )
+                    .success(function (data) {
+                        if (data == 'wrong token') {
+                            $scope.form.password.$setValidity('token', false);
+                            $scope.$broadcast('show-errors-check-validity');
+                        } else {
+                            console.log('success', data);
+                        }
+                    });
             }
         };
     });
